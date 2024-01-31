8e55c1a193d01e95311087e2f6534a67c5362f7d
oussama <oussama.kharbouch.officiel@gmail.com>
2024-01-31 17:52:12 +0100

    1er version

---
+++ customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/CustomerFrontThymeleafAppApplication.java
@@ -0,0 +1,25 @@
+package ma.emsi.customerfrontthymeleafapp;
+
+import ma.emsi.customerfrontthymeleafapp.entities.Customer;
+import ma.emsi.customerfrontthymeleafapp.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerFrontThymeleafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFrontThymeleafAppApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder().name("oussama").email("oussama@gmail.com").build());
+            customerRepository.save(Customer.builder().name("amine").email("amine@gmail.com").build());
+            customerRepository.save(Customer.builder().name("ahmed").email("ahmed@gmail.com").build());
+        };
+    }
+}
